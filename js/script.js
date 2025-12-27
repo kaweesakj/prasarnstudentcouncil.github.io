1130f39199e734eac0082649373d2f07405467d3
kaweesakj <bangkkygamerth@gmail.com>
2025-12-28 01:03:56 +0700

    Update script.js
    Signed-off-by: kaweesakj <bangkkygamerth@gmail.com>

--- js/script.js
+++ js/script.js
@@ -12,3 +12,25 @@ setInterval(() => {
     index = (index + 1) % slides.length;
     slides[index].classList.add("active");
 }, 4000);
+
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("show");
+}
+
+/* ===== Scroll Reveal ===== */
+function revealOnScroll() {
+    const reveals = document.querySelectorAll(".reveal");
+
+    reveals.forEach(el => {
+        const windowHeight = window.innerHeight;
+        const elementTop = el.getBoundingClientRect().top;
+        const revealPoint = 100;
+
+        if (elementTop < windowHeight - revealPoint) {
+            el.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
